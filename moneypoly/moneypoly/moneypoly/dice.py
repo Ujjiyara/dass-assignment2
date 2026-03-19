419eff41e07853ea79b31f342342246d8caecfca
ujiyara <sakshamgoyal.sgm27@gmail.com>
2026-03-19 13:48:00 +0530

    Iteration 6: Added docstring, removed unused import, and fixed attribute initialization in dice.py

--- moneypoly/moneypoly/moneypoly/dice.py
+++ moneypoly/moneypoly/moneypoly/dice.py
@@ -1,5 +1,8 @@
+"""
+Dice module providing the Dice class for simulating random rolls in MoneyPoly.
+"""
 import random
-from moneypoly.config import BOARD_SIZE
+
 
 class Dice:
     """Simulates a pair of six-sided dice with consecutive-doubles tracking."""
@@ -7,6 +10,7 @@ class Dice:
     def __init__(self):
         self.die1 = 0
         self.die2 = 0
+        self.doubles_streak = 0
         self.reset()
 
     def reset(self):
