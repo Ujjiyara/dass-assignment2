2d179510aeba124148aae6c91fcefdc2a093a850
ujiyara <sakshamgoyal.sgm27@gmail.com>
2026-03-22 11:10:10 +0530

    Integration test: race without registered driver

--- integration/tests/test_integration.py
+++ integration/tests/test_integration.py
@@ -8,3 +8,9 @@ def test_integration_driver_race_registration():
     sys.crew.assign_role("Dom", "driver", 90)
     sys.inv.add_car("Charger")
     assert sys.race.enter_race("Street Sprint", "Dom") is True
+
+def test_integration_missing_driver():
+    sys = StreetRaceManager()
+    sys.inv.add_car("Civic")
+    with pytest.raises(ValueError):
+        sys.race.enter_race("Street Sprint", "Brian")
